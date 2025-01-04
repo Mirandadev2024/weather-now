4eeb296f301600a0227231ed69d31ca5219b062f
Mirandadev2024 <mirandaff2023@gmail.com>
2025-01-03 23:07:43 -0300

    resolved conflict in README.md

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+
 # weather-now
 A simple web site that makes a request to open weather map and returns weather data
 I would like to thank OpenAI as part of the code was generated with the help of Chat GPT prompts.
+=======
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+3ec9b06 (Initial commit)
---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+import minhaImagem from './assets/tempo.png';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const api = axios.create({
+  baseURL: 'https://api.openweathermap.org/data/2.5/weather',
+});
+
+class App extends Component {
+  state = {
+    previsoes: [],
+    isLoading:false
+  };
+
+  getPrevisoes = async () => {
+    
+    try {
+
+      const city = document.querySelector('.custom-input').value;
+      this.setState({ previsoes: [], isLoading: true});
+      const { data } = await api.get('/', {
+        params: {
+          q: city,
+          appid: API_KEY,
+        },
+      });
+      this.setState({ previsoes: [data], isLoading:false}); // Wrap data in an array for mapping
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="AppHeader">
+        <img src = {minhaImagem}/>
+        <form form onSubmit={(e) => e.preventDefault()}>
+          <h2 className = "display-data">Retornar a previsão do tempo para sua cidade</h2><br/>
+          <input className = "custom-input" type = "text" placeholder = "Digite sua cidade" height = "50" width = "100"/><br/><br/>
+          <input  className = "custom-input-submit" type = "submit" onClick = {this.getPrevisoes}/><br/>
+        </form>
+
+         <p className = "main-text-style">Clique no botão para saber mais sobreo os dados meteorológicos da sua cidade</p>
+          {this.state.isLoading?(<p className = "carregamento">Carregando dados</p>):(
+          this.state.previsoes.map((previsao) => (
+            <>
+            <h2 className = "display-data" key={previsao.coord.lon}>Temperatura: {Math.round(previsao.main.temp -273,1)}°C</h2><br/>
+            <h2 className = "display-data" key={previsao.coord.lon}>Pressão: {Math.round(previsao.main.pressure,1)}hPa</h2><br/>
+            <h2 className = "display-data" key={previsao.coord.lon}>Umidade: {Math.round(previsao.main.humidity,1)}%</h2>
+            </>
+          )))}
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
